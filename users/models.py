c84ac83ba452fdf1165ec07c29366ead85d4a87c
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-11-29 12:37:14 +0300

    создана модель, применены миграции

--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+NULLABLE = {
+    'blank': True,
+    'null': True,
+}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    first_name = models.CharField(max_length=150, default='anonym', verbose_name='first_name')
+    last_name = models.CharField(max_length=150, default='anonym', verbose_name='last_name')
+    phone = models.CharField(unique=True, max_length=20, verbose_name='phone', **NULLABLE)
+    telegram = models.CharField(unique=True, max_length=100, verbose_name='telegram', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='avatar', **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name='active')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['id']
