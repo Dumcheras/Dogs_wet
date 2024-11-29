ce3b8082d1b149871c3f671c1b8b7683367dca02
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-11-29 17:11:01 +0300

    добавлен функционал регистрации пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+import random
+import string
 
-# Create your views here.
+from django.http import HttpResponseRedirect, HttpResponse
+
+from django.contrib import messages
+from django.shortcuts import render, reverse, redirect
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+
+from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm, UserPasswordChangeForm
+
+
+def user_register_view(request):
+    form = UserRegisterForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            new_user = form.save()
+            new_user.set_password(form.cleaned_data['password2'])
+            new_user.save()
+            return HttpResponseRedirect(reverse('users:user_login'))
+    context = {
+        'title': 'Регистрация пользователя',
+        'form': form,
+    }
+    return render(request, 'users/user_register.html', context)
