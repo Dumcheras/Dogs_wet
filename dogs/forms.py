695d8239bdfce8236b645c3fddb989aa7112610e
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-11-29 19:18:20 +0300

    нормализована форма для создания и обновления собак, добавлена проверка даты рождения собаки

--- dogs/forms.py
+++ dogs/forms.py
@@ -1,8 +1,19 @@
+import datetime
+
 from django import forms
+
 from dogs.models import Dog
+from users.forms import StyleFormMixin
 
 
-class DogForm(forms.ModelForm):
+class DogForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Dog
-        fields = '__all__'
+        exclude = ('owner',)
+
+    def clean_birth_date(self):
+        cleaned_data = self.cleaned_data['birth_date']
+        now_year = datetime.datetime.now().year
+        if now_year - cleaned_data.year > 100:
+            raise forms.ValidationError('Собака должна быть моложе 100 лет')
+        return cleaned_data
