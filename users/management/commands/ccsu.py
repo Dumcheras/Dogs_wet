d4a74f42dc9c189ebfe71f7097d391af48751a56
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-11-29 13:11:16 +0300

    добавлена команда на создание админа

---
+++ users/management/commands/ccsu.py
@@ -0,0 +1,22 @@
+import os
+
+from django.core.management import BaseCommand
+from dotenv import load_dotenv
+from users.models import User
+
+load_dotenv()
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        admin = User.objects.create(
+            email='admin@web.top',
+            first_name='Admin',
+            last_name='Adminov',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True
+        )
+        admin.set_password(os.getenv('SUPERUSER_PASSWORD'))
+        admin.save()
+        print('admin created!')
