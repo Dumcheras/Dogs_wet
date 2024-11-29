e93c8868b8525ace1455e8ab8d55a1077067713a
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-11-29 14:11:07 +0300

    добавлена view главной страницы

--- dogs/views.py
+++ dogs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from dogs.models import Category, Dog
+
+
+def index(request):
+    context = {
+        'category_object_list': Category.objects.all()[:3],
+        'title': 'Питомник - Главная'
+    }
+    return render(request, 'dogs/index.html', context=context)
