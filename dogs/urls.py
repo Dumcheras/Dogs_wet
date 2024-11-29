e297343c4864ffe7648103a92ca63589dbdc300b
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-11-29 14:32:29 +0300

    добавили страницу с породами, прописали урлы, содали страницу

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from dogs.views import index
+from dogs.views import index, categories
 from dogs.apps import DogsConfig
 
 app_name = DogsConfig.name
 
+#пути к страницам на сайте
 urlpatterns = [
     path('', index, name='index'),
+    path('categories/', categories, name='categories'),
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -7,9 +7,18 @@ from django.contrib.auth.decorators import login_required
 from dogs.models import Category, Dog
 
 
+# делаем страницы сайта
 def index(request):
     context = {
         'category_object_list': Category.objects.all()[:3],
         'title': 'Питомник - Главная'
     }
     return render(request, 'dogs/index.html', context=context)
+
+
+def categories(request):
+    context = {
+        'category_object_list': Category.objects.all(),
+        'title': 'Питомник - Все породы'
+    }
+    return render(request, 'dogs/categories.html', context=context)
